d0df7bc912e31acb04bb7f9825e6c7513c24efb0
LebedevOleg <56258311+LebedevOleg@users.noreply.github.com>
2023-03-01 12:36:06 +0300

    + dev04

--- develop/dev04/task.go
+++ develop/dev04/task.go
@@ -1,36 +1,93 @@
 package main
 
+import (
+	"sort"
+	"strings"
+)
+
 type letters struct {
+	word      string
 	allLetter map[rune]int
 }
 
-func (l letters) CheckLetters(word string) bool {
-	for _, v := range word {
-		if count, ok := l.allLetter[v]; ok {
-			if count <= 0 {
-				return false
-			}
-			l.allLetter[v]--
-			if l.allLetter[v] == 0 {
-				delete(l.allLetter, v)
-			}
-			continue
+func NewLetters(w string) letters {
+	l := letters{}
+	l.SetWord(w)
+	return l
+}
+
+func (l *letters) Equal(EL letters) bool {
+	for key, val := range l.allLetter {
+		v, ok := EL.allLetter[key]
+		if !ok {
+			return false
 		}
-		return false
+		if v != val {
+			return false
+		}
+		continue
 	}
 	return true
 }
 
+func (l *letters) SetWord(w string) {
+	l.word = w
+	l.allLetter = make(map[rune]int)
+	for _, v := range w {
+		l.AddLetter(v)
+	}
+}
+
 func (l *letters) AddLetter(char rune) {
-	l.allLetter[char]++
-	/* if _, ok := l.allLetter[char]; ok {
+
+	if _, ok := l.allLetter[char]; ok {
 		l.allLetter[char]++
 		return
 	}
-	l.allLetter[char] = 1 */
+	l.allLetter[char] = 1
 
 }
 
-func SearchAnagramms(words []string) map[string][]string {
+func SortMap(m map[string][]string) {
+	for key, val := range m {
+		if len(val) <= 1 {
+			delete(m, key)
+		}
+		sort.Strings(val)
+	}
+}
+func ToLowerCase(words []string) {
+	for i, v := range words {
+		words[i] = strings.ToLower(v)
+	}
+}
 
+func SearchAnagramms(words []string) map[string][]string {
+	wordsArr := make([]letters, 0)
+	ToLowerCase(words)
+	anagrammsArr := make(map[string][]string)
+	addCheck := false
+	for _, v := range words {
+		lword := NewLetters(v)
+		for j := 0; j < len(wordsArr); j++ {
+			if wordsArr[j].word == "" {
+				break
+			}
+			if lword.Equal(wordsArr[j]) {
+				if lword.word != wordsArr[j].word {
+					anagrammsArr[wordsArr[j].word] = append(anagrammsArr[wordsArr[j].word], v)
+				}
+				addCheck = !addCheck
+				break
+			}
+		}
+		if _, key := anagrammsArr[v]; !key && !addCheck {
+			wordsArr = append(wordsArr, NewLetters(v))
+			anagrammsArr[v] = []string{}
+			continue
+		}
+		addCheck = !addCheck
+	}
+	SortMap(anagrammsArr)
+	return anagrammsArr
 }
--- develop/dev04/task_test.go
+++ develop/dev04/task_test.go
@@ -1,1 +1,9 @@
 package main
+
+import "testing"
+
+func TestAnagramms(t *testing.T) {
+	testWords := []string{"пятак", "сЛиток", "тяпкА", "пятак", "листок", "пятка", "столик", "ключ"}
+	testResult := SearchAnagramms(testWords)
+	t.Log(testResult)
+}
